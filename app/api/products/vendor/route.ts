621dd6650ddaac5cddc1b2f4afb460fffc888313
Asuno <cheickoumar871@gmail.com>
2025-05-27 16:44:52 +0200

    ficing staic pages bugs

--- app/api/products/vendor/route.ts
+++ app/api/products/vendor/route.ts
@@ -1,3 +1,7 @@
+// app/api/products/vendor/route.ts
+export const dynamic = "force-dynamic";
+
+
 import { NextResponse } from "next/server"
 
 import { db } from "@/lib/db"
--- app/api/search/route.ts
+++ app/api/search/route.ts
@@ -1,6 +1,11 @@
+// app/api/products/vendor/route.ts
+
+
+
 import { NextResponse } from "next/server"
 
 import { db } from "@/lib/db"
+   export const dynamic = 'force-dynamic';
 
 export async function GET(req: Request) {
   try {
